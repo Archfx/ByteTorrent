084de079dea2eca6a302b2de1d9d7405f014b4b3
archfx <arunajayasena@ufl.edu>
2021-03-10 12:50:33 -0500

    choke moved to seperate file. Choke utilizes peer.peerstateMgr

--- Client/src/main/java/peer/PeerStateManager.java
+++ Client/src/main/java/peer/PeerStateManager.java
@@ -3,6 +3,7 @@ package peer;
 import peer.Peer;
 
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.*;
 
 public class PeerStateManager extends Thread {
@@ -13,9 +14,21 @@ public class PeerStateManager extends Thread {
     private static ArrayList<Peer> interestedPeers = new ArrayList<Peer>();
 	private static ArrayList<Peer> kNeighborPeers;
 
+    public Object lockMyNeighbors;
+    public int numNeighbors;
+    public int[] neighbors;
+    public boolean[] isInterested;
+    public boolean isMyFile;
+    public boolean dlFinished;
+    public int unchokeInterval;
+
+    public int[] allPeerID;
+    public Socket[] allPeerSockets;
+
     public PeerStateManager(ServerSocket ssocket, Peer hostPeer, HashMap<Integer, Peer> peers) {
 		this.ssocket = ssocket;
 		this.peers = peers;
 		this.hostPeer = hostPeer;
 	}
 }
+
