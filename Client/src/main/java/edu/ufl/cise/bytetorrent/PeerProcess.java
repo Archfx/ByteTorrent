b1c9b4be6645a2e250450d8281bd95fb6c9a2d95
Hansika <hansikam94@gmail.com>
2021-04-19 01:16:33 -0400

    refactor PeerProcess

--- Client/src/main/java/edu/ufl/cise/bytetorrent/PeerProcess.java
+++ Client/src/main/java/edu/ufl/cise/bytetorrent/PeerProcess.java
@@ -3,7 +3,6 @@ package edu.ufl.cise.bytetorrent;
 import edu.ufl.cise.bytetorrent.config.CommonConfig;
 import edu.ufl.cise.bytetorrent.config.PeerInfoConfig;
 import edu.ufl.cise.bytetorrent.model.Peer;
-import edu.ufl.cise.bytetorrent.service.ChokeManagementService;
 import edu.ufl.cise.bytetorrent.util.LoggerUtil;
 
 import java.util.HashMap;
@@ -13,43 +12,24 @@ public class PeerProcess {
     public static void main(String[] args) {
 
         LoggerUtil.LogInfoMessage("Process Thread Started");
-        // System.out.println(port);
-
-        
-        // Get the peer id from arguments
-         final int id = Integer.parseInt(args[0]);
-
-
-        // Read Configurations from edu.ufl.cise.bytetorrent.config files
+        final int id = Integer.parseInt(args[0]);
         CommonConfig commonConfig = CommonConfig.getInstance();
         PeerInfoConfig peerInfo = PeerInfoConfig.getInstance();
-
-        // Get my peer information
         Peer my_node = null;
 
-        // Assign existing remote peers
         Map<Integer,Peer> remotePeers = new HashMap<>();
-
-        ChokeManagementService chokeM = new ChokeManagementService();
-
 
         for (Peer peer : peerInfo.getPeerInfoList()) {
             if (id == peer.getPeerId()) {
                 my_node = peer;
             }
             else {
-                // Add all other peers
                 remotePeers.put(peer.getPeerId(), peer);
             }
         }
 
         PeerManagerPlatform peerProcess = new PeerManagerPlatform(my_node, remotePeers, commonConfig);
 		peerProcess.init();
-
-        // peerStateManagerPlatform = new PeerStateManagerPlatform(my_node, remotePeers);
-//        System.out.println(" Starting choke "+ chokeM.choke(remotePeers));
-        // peerStateManagerPlatform.init();
-        
 
     }
 }
